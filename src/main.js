8f6d9f01190e4ed40f791ae575ec435bce4bbbe8
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2024-06-19 18:49:43 +0300

    make task

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
+import { getImage } from './js/pixabay-api.js';
+import { marcup, showLoader, hideLoader } from './js/render-function.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+export const refs = {
+  form: document.querySelector('.form'),
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  const value = e.target.elements.search.value;
+  if (value === '') {
+    refs.gallery.innerHTML = ' ';
+    iziToast.warning({
+      title: 'warning',
+      message: ' Enter a word for the query, please.',
+      position: 'center',
+      displayMode: 'once',
+      maxWidth: 500,
+      imageWidth: 600,
+    });
+  }
+
+  showLoader();
+  const arr = getImage(value);
+  if (arr.length !== 0) {
+    arr.then(data => marcup(data.hits));
+  }
+  arr.catch(err => {
+    iziToast.show({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+      displayMode: 'once',
+      maxWidth: 500,
+      imageWidth: 600,
+    });
+  });
+  arr.finally(() => {
+    hideLoader();
+    refs.form.reset();
+  });
+});
